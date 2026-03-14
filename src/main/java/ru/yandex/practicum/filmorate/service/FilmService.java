7fd868dfeb735957a16a9486a46523a6e9ddf360
RodionovaAP <rodionovaap@admlr.lipetsk.ru>
2026-03-14 16:18:39 +0300

    fix: logic service&controller

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -8,7 +8,6 @@ import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 @Service
