e15324742d73fc5dc5e7a3bb8614b0bfd01afb61
RodionovaAP <rodionovaap@admlr.lipetsk.ru>
2026-03-28 12:18:25 +0300

    feat: sprint 12

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserDbStorage.java
@@ -91,28 +91,15 @@ public class UserDbStorage implements UserStorage {
 
     @Override
     public List<User> getFriends(Long userId) {
-        String sql = """
-                    SELECT u.* 
-                    FROM users u
-                    JOIN friends f ON u.id = f.friend_id
-                    WHERE f.user_id = ?
-                """;
+        String sql = "SELECT u.* FROM users u join friends f ON u.id = f.friend_id WHERE f.user_id = ?";
+
 
         return jdbcTemplate.query(sql, new UserRowMapper(), userId);
     }
 
     @Override
     public List<User> getCommonFriends(Long userId, Long otherId) {
-        String sql = """
-                    SELECT u.* 
-                    FROM users u
-                    WHERE u.id IN (
-                        SELECT f1.friend_id
-                        FROM friends f1
-                        JOIN friends f2 ON f1.friend_id = f2.friend_id
-                        WHERE f1.user_id = ? AND f2.user_id = ?
-                    )
-                """;
+        String sql = "SELECT u.* FROM users u WHERE u.id IN (SELECT f1.friend_id FROM friends f1 JOIN friends f2 ON f1.friend_id = f2.friend_id WHERE f1.user_id = ? AND f2.user_id = ?)";
 
         return jdbcTemplate.query(sql, new UserRowMapper(), userId, otherId);
     }
